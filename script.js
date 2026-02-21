e93ae8dea7e4e87dc49ac48cfeb034ce0b616ca5
supraebapak <supraebapak456@gmail.com>
2026-02-21 20:04:58 +0700

    New Website

---
+++ script.js
@@ -0,0 +1,13 @@
+const humburger = document.querySelector("#humburger-menu");
+const navbarNav = document.querySelector("#List");
+
+humburger.addEventListener("click", function(e) {
+  e.preventDefault();
+  navbarNav.classList.toggle("active");
+});
+
+document.addEventListener("click", function(e) {
+  if (!navbarNav.contains(e.target) && !humburger.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
